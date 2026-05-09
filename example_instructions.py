bc609d0ffa83b194847d92b5dac87eda8594c71c
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-05-08 21:51:50 -0700

    Add example_instructions.py from codellama

--- example_instructions.py
+++ example_instructions.py
@@ -7,7 +7,6 @@ import fire
 
 from llama import Llama
 
-
 def main(
     ckpt_dir: str,
     tokenizer_path: str,
@@ -57,9 +56,9 @@ def main(
 
     for instruction, result in zip(instructions, results):
         for msg in instruction:
-            print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+            print(f"{msg[role].capitalize()}: {msg[content]}\n")
         print(
-            f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+            f"> {result[generation][role].capitalize()}: {result[generation][content]}"
         )
         print("\n==================================\n")
 
